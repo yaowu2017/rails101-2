a0bbb903df68ce42adc3d7f2204292e63c2fb746
yaowu2017 <shayazhu911@gmail.com>
2017-04-05 22:07:21 +0800

    implement edit function on  my posts

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -3,4 +3,15 @@ before_action :authenticate_user!
  def index
    @posts = current_user.posts
  end
+ def edit
+   @post = Post.find(params[:id])
+   posts = current_user.posts
+ end
+ def destroy
+   @post = Post.find(params[:id])
+
+   @post.destroy
+   flash[:alert] = "Post deleted"
+   redirect_to posts_path
+ end
 end
